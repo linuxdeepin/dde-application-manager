1e1ea2594c6a648b8c282953471fb6c7b5129c8f
black-desk <me@black-desk.cn>
2023-10-17 11:25:48 +0800

    fix: include QStringBuilder and improve api doc
    log:

--- src/desktopfileparser.cpp
+++ src/desktopfileparser.cpp
@@ -3,6 +3,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
 #include <QRegularExpression>
+#include <QStringBuilder>
+
 #include "desktopfileparser.h"
 #include "constant.h"
 #include "global.h"
