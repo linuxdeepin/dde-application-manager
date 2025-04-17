fcd03531eb4c9263d375740d211ed01f87c9731b
Wang Zichong <wangzichong@deepin.org>
2025-04-17 17:25:31 +0800

    Revert "fix: 修复在grub界面选择回退到之前的系统后，桌面上的图标显示为齿轮图标问题"
    This reverts commit 237b5624e69150b712229c1fd65e11a43cf0710a.
    
    PMS: BUG-313269

--- src/dbus/applicationmanager1service.h
+++ src/dbus/applicationmanager1service.h
@@ -46,7 +46,6 @@ public:
     [[nodiscard]] QHash<QDBusObjectPath, QSharedPointer<ApplicationService>>
     findApplicationsByIds(const QStringList &appIds) const noexcept;
     void updateApplication(const QSharedPointer<ApplicationService> &destApp, DesktopFile desktopFile) noexcept;
-    void autoRemoveFromDesktop() noexcept;
 
     [[nodiscard]] const auto &Applications() const noexcept { return m_applicationList; }
     [[nodiscard]] JobManager1Service &jobManager() noexcept { return *m_jobManager; }
